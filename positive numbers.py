4b39060dd5dc1d1ca5952f3210d0a2de69ec9e0f
Raguraman18 <97165981+Raguraman18@users.noreply.github.com>
2022-01-24 14:24:42 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,9 @@
+list1=[12,-7,5,64,-14]
+n= [num for num in list1 if num>=0]
+print("Positive numbers are;",n)
+
+list2=[12,14,-95,3]
+m= [num for num in list2 if num>=0]
+print("Positive numbers are",m)
+        
+        
